bb3d0225d4be02b4cb5c04d0c49e9718c8195287
Jefferson Ben Lee <62727049+bezn777@users.noreply.github.com>
2020-05-12 10:50:41 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,14 @@
+print('Welcome to the world')
+print('We will be calculating some variable')
+y = input('Enter a number:' )
+y = int(y)
+x =  3
+z = x * y
+
+if z > 2 : 
+    print('I love you!')
+elif z > 10:
+    print('You are beautiful')
+else :
+    print('Stupid')
+
